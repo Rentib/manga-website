459cc1183e130cfe37ce14b941f14b63d87c3b5c
Rentib <70008415+Rentib@users.noreply.github.com>
2020-08-21 09:40:10 +0200

    Add files via upload

---
+++ manga_javascript.js
@@ -0,0 +1,41 @@
+/*----------------SORTOWANIE LISTY----------------------------------------------------------------------*/
+window.onload = function sortList() {
+  var list, i, switching, b, shouldSwitch, k;
+  list = document.getElementById("lista");
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    k = list.getElementsByClassName("tytul");
+    for (i = 0; i < (k.length - 1); i++) {
+      shouldSwitch = false;
+      var s1 = k[i].innerHTML.toLowerCase();
+      var s2 = k[i + 1].innerHTML.toLowerCase()
+      s1 = s1.replace(/\s/g, '');
+      s2 = s2.replace(/\s/g, '');
+      if (s1 > s2) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+/*----------------SCROLL FUNCTION-----------------------------------------------------------------------*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    scrollFunctionMenu();
+}
+/*----------------MENU HEIGHT CHANGE--------------------------------------------------------------------*/
+function scrollFunctionMenu() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 50) {
+        document.getElementById("siteHeader").style.height = "5%";
+    } else {
+        document.getElementById("siteHeader").style.height = "7%";
+    }
+}
+/*------------------------------------------------------------------------------------------------------*/
